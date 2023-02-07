8da1f909a170c0841de3227895a5ac15cc3bf781
MrAbuz <miguelmrosario@gmail.com>
2023-02-07 23:12:23 +0000

    07/02/2023

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 What we've did so far to setup The Graph:
 
-Creating a subgraph on the The Graph website
-All the stuff in the beginning of installing cli, and initializing that we have the commands in the website
-Downloaded the graphql extension that is on github
-Updated the schema.graphql
-Called "graph codegen" after we updated the schema.graphql
-And now we're updating the "nft-marketplace.ts" file from the "src" folder
+- Creating a subgraph on the The Graph website
+- All the stuff in the beginning of installing cli, and init that we have the commands in the website (all options of the init were 'yes', startBlock=0 etc)
+- Downloaded the graphql extension that is on github
+- Updated the schema.graphql
+- Called "graph codegen" after we updated the schema.graphql
+- And now we updated the "nft-marketplace.ts" file from the "src" folder
+- In the end we updated the subgraph.yaml in just 1 of the options: in the "startBlock". If it started from the startBlock=0 it would start indexing events from the beginning of Ethereum which would take a lot of time to run. So we copy pasted our contract goerli address to the goerli etherscan and saw in which block did we deploy the contract. We copied the block and took 1 block from it, so it will start counting from the block before the one our contract was deployed. (ex: our contract was deployed in block 8433646, so we defined startBlock=8433645)
+- Deployed the subgraph (following the steps that we have in the Subgraph Studio), easy
+
+So, we only updated:
+
+- Schema.graphql
+- src/nft-marketplace.ts
+- Just 1 little update in subgraph.yaml that I explained above
+
+All of this was super easy and super intuitive to make. Only thing we'll have to figure out in another project will be that logic that we used to create the table for activeItem to keep track of the nfts that we have visibly listed on the website, which they will only show as listed if they are address 0x000, and if a user cancels the listing it will have the 0x00dEaD address and if a user buys it, it will have the buyers address, so in those last 2 cases the nft wont have the 0x000 address so will stop being shown in the nft marketplace.
+I'm assuming other projects may need similar or different logic for any logic they wanna create, or maybe it just updates each event object and its super straight forward and easy to code.
--- src/nft-marketplace.ts
+++ src/nft-marketplace.ts
@@ -1,4 +1,7 @@
 // Its in this "nft-marketplace.ts" file from the src that we're gonna tell our subgraph how to map and store all the event information
+// Super easy the syntax and easy to do all this The Graph code from the beginning to the end
+// Read the README.md
+
 import { BigInt, Address } from "@graphprotocol/graph-ts"; // two special types that come from The Graph
 import {
   ItemBought as ItemBoughtEvent,
@@ -10,7 +13,46 @@ import {
   ItemBought,
   ItemCanceled,
   ItemListed,
-} from "../generated/schema"; //this is why we do "graph codegen" like I said in schema.graphql file im assuming. We have to import those types from there
+} from "../generated/schema"; //this is why we do "graph codegen" like I said in schema.graphql file im assuming. We have to import those types from generated/schema
+
+export function handleItemListed(event: ItemListedEvent): void {
+  let itemListed = ItemListed.load(
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  );
+  let activeItem = ActiveItem.load(
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  );
+  if (!itemListed) {
+    itemListed = new ItemListed(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+    );
+  }
+  if (!activeItem) {
+    //since this ItemListed event is also used for update listing, in that case there's already an activeItem. If its emited because a listing was created, there wont be an activeItem.
+    activeItem = new ActiveItem(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+    );
+  }
+  itemListed.seller = event.params.seller;
+  activeItem.seller = event.params.seller;
+
+  itemListed.nftAddress = event.params.nftAddress;
+  activeItem.nftAddress = event.params.nftAddress;
+
+  itemListed.tokenId = event.params.tokenId;
+  activeItem.tokenId = event.params.tokenId;
+
+  itemListed.price = event.params.price;
+  activeItem.price = event.params.price;
+
+  activeItem.buyer = Address.fromString(
+    //so we initialize the buyer as the 0x000 address
+    "0x0000000000000000000000000000000000000000"
+  );
+
+  itemListed.save();
+  activeItem.save();
+}
 
 export function handleItemBought(event: ItemBoughtEvent): void {
   // This functions are basically saying: whenever an "ItemBoughtEvent" occurs, do this "handleItemBought()" function
@@ -42,19 +84,52 @@ export function handleItemBought(event: ItemBoughtEvent): void {
   itemBought.nftAddress = event.params.nftAddress;
   itemBought.tokenId = event.params.tokenId;
   // Only thing we have to update activeItem is the buyer:
+  // Here with The Graph we're not gonna delete the activeItem (like in Moralis).
+  // We're just gonna say: If it has a buyer, it means it's been bought, if not, it's still on the market.
   activeItem!.buyer = event.params.buyer; // ! is some typescript stuff which means we will have an activeItem, dont need to worry too much about it if we unfamiliar with typescript
 
   itemBought.save();
   activeItem!.save();
   // And this is how we're gonna save this itemBought event as an object in The Graph protocol, and also update out activeItem
-  // Here with The Graph we're not gonna delete the activeItem (like in Moralis). We're just gonna say: If it has a buyer, it means it's been bought, if not, it's still on the market.
 }
-export function handleItemCanceled(event: ItemCanceledEvent): void {}
+export function handleItemCanceled(event: ItemCanceledEvent): void {
+  let itemCanceled = ItemCanceled.load(
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  );
+  let activeItem = ActiveItem.load(
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  );
+  if (!itemCanceled) {
+    //which there shouldnt be
+    itemCanceled = new ItemCanceled(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+    );
+  }
 
-export function handleItemListed(event: ItemListedEvent): void {}
+  itemCanceled.seller = event.params.seller;
+  itemCanceled.nftAddress = event.params.nftAddress;
+  itemCanceled.tokenId = event.params.tokenId;
+
+  //we're gonna give it what is called, the dead address
+  activeItem!.buyer = Address.fromString(
+    "0x000000000000000000000000000000000000dEaD"
+  );
+
+  itemCanceled.save();
+  activeItem!.save();
+}
 
 function getIdFromEventParams(tokenId: BigInt, nftAddress: Address): string {
+  //this is the ID
   //the combination of "tokenId" and "nftAddress" will give an unique Id for each of the events (hmm, is it unique if there's more calls to that tokenId of that nft address?)
   //he said "and tokenId has a function called .toHexString()"
   return tokenId.toHexString() + nftAddress.toHexString();
 }
+
+//So, when its listed, we initialize the buyer in the ActiveItem object as the 0x0000 address.
+//    when its bought, we update the buyer with the buyers address.
+//    when its canceled, we update the buyer with the 0x000dEaD address
+
+//So instead of deleting the ActiveItem object when its bought or canceled (like we did in Moralis), if its 0x000 address its good and keeps being listed, if there's a buyer address
+//we'll remove it from listing, and if it has the 0x000deAd address we remove it from listing aswell
+//Basically, it will only be listed and on the market if the buyers address is just zeros "0x000000..."
